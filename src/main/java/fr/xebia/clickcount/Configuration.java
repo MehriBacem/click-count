73044c68d84b78ba464d2bf5c2522f07f8944adf
MehriBacem <bacemmehrii@gmail.com>
2020-10-06 23:14:50 +0200

    create env variable for redis name

--- src/main/java/fr/xebia/clickcount/Configuration.java
+++ src/main/java/fr/xebia/clickcount/Configuration.java
@@ -10,7 +10,7 @@ public class Configuration {
     public final int redisConnectionTimeout;  //milliseconds
 
     public Configuration() {
-        redisHost = "redis";
+        redisHost =  (String) System.getenv().get("REDIS_HOST");
         redisPort = 6379;
         redisConnectionTimeout = 2000;
     }
